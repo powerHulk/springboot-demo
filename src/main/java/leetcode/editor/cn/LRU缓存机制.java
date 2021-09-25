f007043f9af76bfb221ae8a49b5ac3ee7effc7bc
powerhulk <powerhulk@qq.com>
2021-09-25 22:02:04 +0800

    lru

---
+++ src/main/java/leetcode/editor/cn/LRU缓存机制.java
@@ -0,0 +1,152 @@
+package leetcode.editor.cn;
+
+//运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制 。 
+//
+// 
+// 
+// 实现 LRUCache 类： 
+//
+// 
+// LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存 
+// int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。 
+// void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上
+//限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。 
+// 
+//
+// 
+// 
+// 
+//
+// 进阶：你是否可以在 O(1) 时间复杂度内完成这两种操作？ 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入
+//["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+//[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+//输出
+//[null, null, null, 1, null, -1, null, -1, 3, 4]
+//
+//解释
+//LRUCache lRUCache = new LRUCache(2);
+//lRUCache.put(1, 1); // 缓存是 {1=1}
+//lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+//lRUCache.get(1);    // 返回 1
+//lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+//lRUCache.get(2);    // 返回 -1 (未找到)
+//lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+//lRUCache.get(1);    // 返回 -1 (未找到)
+//lRUCache.get(3);    // 返回 3
+//lRUCache.get(4);    // 返回 4
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= capacity <= 3000 
+// 0 <= key <= 10000 
+// 0 <= value <= 10⁵ 
+// 最多调用 2 * 10⁵ 次 get 和 put 
+// 
+// Related Topics 设计 哈希表 链表 双向链表 👍 1636 👎 0
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRU缓存机制 {
+    public static void main(String[] args) {
+        LRUCache lRUCache = new LRU缓存机制().new LRUCache(2);
+        lRUCache.put(1, 1); // 缓存是 {1=1}
+        lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+        System.out.println(lRUCache.get(1));// 返回 1
+        lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+        System.out.println(lRUCache.get(2)); // 返回 -1 (未找到)
+        lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+        System.out.println(lRUCache.get(1));    // 返回 -1 (未找到)
+        System.out.println(lRUCache.get(3));    // 返回 3
+        System.out.println(lRUCache.get(4));    // 返回 4
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class LRUCache {
+
+        private Map<Integer, Node> map = new HashMap<>();
+        private int capacity;
+        private Node head = new Node();
+        private Node tail = new Node();
+
+        public LRUCache(int capacity) {
+            this.capacity = capacity;
+            head.next = tail;
+            tail.prev = head;
+        }
+
+        public int get(int key) {
+            if (map.containsKey(key)) {
+                Node node = map.get(key);
+                deleteNode(node);
+                moveToHead(node);
+                return node.value;
+            }
+            return -1;
+        }
+
+        public void put(int key, int value) {
+            if (get(key) > -1) {
+                Node node = map.get(key);
+                node.value = value;
+                deleteNode(node);
+                moveToHead(node);
+            } else {
+                Node node = new Node(key, value);
+                if (map.size() == capacity) {
+                    map.remove(tail.prev.key);
+                    deleteNode(tail.prev);
+                }
+                moveToHead(node);
+                map.put(key, node);
+            }
+        }
+
+        private void deleteNode(Node node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+
+        private void moveToHead(Node node) {
+            node.prev = head;
+            node.next = head.next;
+            head.next = node;
+            node.next.prev = node;
+        }
+
+        class Node {
+            int key, value;
+            Node prev, next;
+
+            public Node() {
+            }
+
+            public Node(int key, int value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+    }
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
