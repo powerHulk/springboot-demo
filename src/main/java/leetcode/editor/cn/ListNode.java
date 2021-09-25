9e83e6d022a8fa9063060825fb3d2aa0a5b25147
powerhulk <powerhulk@qq.com>
2021-09-25 18:48:51 +0800

    delete

--- src/main/java/leetcode/editor/cn/ListNode.java
+++ src/main/java/leetcode/editor/cn/ListNode.java
@@ -1,5 +1,9 @@
 package leetcode.editor.cn;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author: zhangth
  * @CreateDate: 2021/9/15
@@ -18,5 +22,30 @@ public class ListNode {
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    public static ListNode arrayToNode(int[] nums) {
+        ListNode head = null, curr = null;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                head = new ListNode(nums[i]);
+                curr = head;
+            } else {
+                ListNode node = new ListNode(nums[i]);
+                curr.next = node;
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+
+    public static void nodeToArray(ListNode head) {
+        List result = new ArrayList();
+        ListNode curr = head;
+        while (curr != null) {
+            result.add(curr.val);
+            curr = curr.next;
+        }
+        System.out.println(Arrays.toString(result.toArray()));
     }
 }
